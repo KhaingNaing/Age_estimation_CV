b0f28f481a5c8d3888cedf5aa994405fa47bb7cb
KhaingNaing <kmyatnoe7889@gmail.com>
2024-07-24 20:02:49 +1000

    adding option for a 1 million constrain param simple CNN

--- model.py
+++ model.py
@@ -5,13 +5,13 @@ from config import config
 
 # Custom Simple CNN model 
 class SimpleCNN(nn.Module):
-    def __init__(self, input_dim, output_nodes, model_name='simple_cnn'):
+    def __init__(self, input_dim, output_nodes, model_name="simple_cnn"):
         super(SimpleCNN, self).__init__()
 
         self.input_dim = input_dim
         self.output_nodes = output_nodes
 
-        if model_name == 'simple_cnn':
+        if model_name == "simple_cnn":
             self.model = nn.Sequential(
                 # 1st Convolutional Block
                 nn.Conv2d(input_dim, 16, kernel_size=3, stride=1, padding=1),
@@ -32,11 +32,38 @@ class SimpleCNN(nn.Module):
                 # Flatten
                 nn.Flatten(),
                 # 1st Fully Connected Layer
-                nn.Linear(128 * (config['img_size'] // 16) * (config['img_size'] // 16), 64),
+                nn.Linear(128 * (config["img_size"]  // 16) * (config["img_size"] // 16), 64),
                 nn.ReLU(inplace=True),
                 # 2nd Fully Connected Layer
                 nn.Linear(64, output_nodes),
             )
+        elif model_name == "large_cnn":
+            self.model = nn.Sequential(
+                # 1st Convolutional Block
+                nn.Conv2d(input_dim, 32, kernel_size=3, stride=1, padding=1),  # Increased from 8
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                # 2nd Convolutional Block
+                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # Increased from 16
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                # 3rd Convolutional Block
+                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # Increased from 32
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                # 4th Convolutional Block
+                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),  # Increased from 64
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                # Flatten
+                nn.Flatten(),
+                # 1st Fully Connected Layer
+                nn.Linear(256 * (config["img_size"] // 16) * (config["img_size"] // 16), 512),  # Increased from 128
+                nn.ReLU(inplace=True),
+                # 2nd Fully Connected Layer
+                nn.Linear(512, output_nodes),  # Added an extra layer to increase parameter count
+            )
+
         else:
             raise ValueError(f"Unsupported model name: {model_name}!")
 
